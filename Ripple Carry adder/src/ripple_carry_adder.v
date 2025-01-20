f93dc65c6ff5c14183a1ac99029ba64c91d56169
Chandan M <chandanmchandum2@gmail.com>
2025-01-20 22:25:45 +0530

    Update ripple_carry_adder.v

--- Ripple Carry adder/src/ripple_carry_adder.v
+++ Ripple Carry adder/src/ripple_carry_adder.v
@@ -1,4 +1,3 @@
-
 module Ripple_carry_adder(input [3:0]A,[3:0]B,Cin,
 output wire [3:0]sum,Cout);
 wire c0,c1,c2;
@@ -6,5 +5,4 @@ wire c0,c1,c2;
     full_adder f1(A[1],B[1],c0,sum[1],c1);
     full_adder f2(A[2],B[2],c1,sum[2],c2);
     full_adder f3(A[3],B[3],c2,sum[3],Cout);
-    
 endmodule
