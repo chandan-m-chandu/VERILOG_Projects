bc3db90e44a78daa6d38788dbe24a4b39705d239
Chandan M <chandanmchandum2@gmail.com>
2025-07-22 10:18:17 +0530

    Update SIPO.v

--- Shift_registers/SIPO/SIPO.v
+++ Shift_registers/SIPO/SIPO.v
@@ -1,24 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.07.2025 15:02:23
-// Design Name: 
-// Module Name: SIPO
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
-
 
 module SIPO(input clk, input rst, 
 input ready,
